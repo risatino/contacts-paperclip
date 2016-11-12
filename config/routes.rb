66accc1c6e2aa36c4af68ffb7ec36f7a82ff67cc
risatino <charissatolentino@gmail.com>
2016-11-12 12:44:01 -0600

    working paperclip test

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :contacts
 
-  get '/' => 'documents#new'
+  get '/' => 'contacts#index'
+  get '/test' => 'contacts#test'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
